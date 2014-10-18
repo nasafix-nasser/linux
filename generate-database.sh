e1e352f5e1120f7f046e3a6e6498f591b7943481
Nasser Afshin <nasser.afshin@gmail.com>
2014-10-18 17:04:10 +0330

    Generate cscope database for ARM-SMDK platform

---
+++ generate-database.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+LNX=/home/smart/Projects/LinuxKernel.org/linux
+#CSCOPE_DB_DIR=/home/smart/Projects/LinuxKernel.org/cscope-database
+CSCOPE_DB_DIR=$LNX
+cd / 	
+find $LNX   \
+	-path "$LNX/arch/arm/mach-*" ! -path "$LNX/arch/arm/mach-s5pv210*" -prune -o          	\
+	-path "$LNX/arch/arm/plat-*" ! -path "$LNX/arch/arm/plat-samsung*" ! -path "$LNX/arch/arm/plat-s5p*" -prune -o	\
+	-path "$LNX/arch/*" ! -path "$LNX/arch/arm*" -prune -o          	\
+	-name "*.[chxsS]" -print > $CSCOPE_DB_DIR/cscope.files
+
+# Generate the database
+cd $CSCOPE_DB_DIR
+cscope -b -q -k
+
+# Now you can use the standalone cscope browser: cscope -d
+# And exit using: CTRL+D
