8d86c4791ecb31dfda4241196fadc228af5b9751
Nasser-Smart-T410 <me@nasserdomain.net>
2014-10-20 07:43:52 +0330

    Add cscope database generator script

---
+++ generate_cscope_database.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+LNX=/home/smart/Projects/LinuxKernel.org/linux
+cd /	
+#find  $LNX                                                                \
+#	-path "$LNX/arch/*" ! -path "$LNX/arch/i386*" -prune -o               \
+#	-path "$LNX/include/asm-*" ! -path "$LNX/include/asm-i386*" -prune -o \
+#	-path "$LNX/tmp*" -prune -o                                           \
+#	-path "$LNX/Documentation*" -prune -o                                 \
+#	-path "$LNX/scripts*" -prune -o                                       \
+#	-path "$LNX/drivers*" -prune -o                                       \
+#	-name "*.[chxsS]" -print >$LNX/cscope.files
+#find $LNX						\
+#	-path "$LNX/arch/arm/*" ! -path "$LNX/arch/arm/mach-*" ! -path "$LNX/arch/arm/plat-*" -name "*.[chxsS]" -o          \
+#	-path "$LNX/arch/arm/mach-s5pv210*"  -name "*.[chxsS]" -o		\
+#	-path "$LNX/arch/arm/plat-samsung/*" -name "*.[chxsS]" -o	\
+#	-path "$LNX/arch/arm/plat-s5p/*" -name "*.[chxsS]" \
+#	>$LNX/cscope.files
+find  $LNX                                                                \
+	-path "$LNX/arch/arm/mach-*" ! -path "$LNX/arch/arm/mach-s5pv210*" -prune -o               \
+	-path "$LNX/arch/arm/plat-*" ! -path "$LNX/arch/arm/plat-samsung*" ! -path "$LNX/arch/arm/plat-s5p*" -prune -o \
+	-path "$LNX/arch/*" ! -path "$LNX/arch/arm*" -prune -o \
+	-path "$LNX/arch/arm64*" -prune -o \
+	-name "*.[chxsS]" -print >$LNX/cscope.files
+
+cd $LNX     # the directory with 'cscope.files'
+cscope -b -q -k
