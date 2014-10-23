2e87e1c47cff9f8317e8063a73cb3228d9d2f15d
Nasser-Smart-T410 <afshin.nasser@gmail.com>
2014-10-23 07:54:10 +0330

    Add a driver for GPIO on Cortex-PC104 board

--- generate-database.sh
+++ generate-database.sh
@@ -7,6 +7,7 @@ find $LNX   \
 	-path "$LNX/arch/arm/mach-*" ! -path "$LNX/arch/arm/mach-s5pv210*" -prune -o          	\
 	-path "$LNX/arch/arm/plat-*" ! -path "$LNX/arch/arm/plat-samsung*" ! -path "$LNX/arch/arm/plat-s5p*" -prune -o	\
 	-path "$LNX/arch/*" ! -path "$LNX/arch/arm*" -prune -o          	\
+	-path "$LNX/arch/arm64*" -prune -o \
 	-name "*.[chxsS]" -print > $CSCOPE_DB_DIR/cscope.files
 
 # Generate the database
